26e099816de3a6ab53a3961b91daa332b400f978
Simon Demuynck <demuyncks1999@gmail.com>
2022-01-10 16:25:26 +0100

    Simple UI update and file selecting

--- vRidance/vRidance/MainWindow.xaml.cs
+++ vRidance/vRidance/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace vRidance
 {
@@ -23,17 +25,65 @@ namespace vRidance
         public MainWindow()
         {
             InitializeComponent();
+            btnNext.IsEnabled = false;
+            btnNext.Visibility = Visibility.Hidden;
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             grdMain.Background = Brushes.White;
+            btnUpload.Background = Brushes.White;
+            btnUpload.Foreground = Brushes.Black;
+            lstItems.Background = Brushes.LightGray;
+            lstItems.Foreground = Brushes.Black;
+            btnUpload.BorderBrush = Brushes.Black;
+            btnNext.Background = Brushes.White;
+            btnNext.Foreground = Brushes.Black;
+            btnNext.BorderBrush = Brushes.Black;
         }
 
         private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
             var bc = new BrushConverter();
-            grdMain.Background = (Brush)bc.ConvertFrom("#1E1E1E");
+            grdMain.Background = (Brush)bc.ConvertFrom("#FF252526");
+            btnUpload.Background = (Brush)bc.ConvertFrom("#FF353434");
+            btnUpload.Foreground = Brushes.White;
+            lstItems.Background = (Brush)bc.ConvertFrom("#FF1E1E1E");
+            lstItems.Foreground = Brushes.White;
+            btnUpload.BorderBrush = null;
+
+            btnNext.Background = (Brush)bc.ConvertFrom("#FF353434");
+            btnNext.Foreground = Brushes.White;
+            btnNext.BorderBrush = null;
+        }
+
+        private void btnUpload_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> itemList = new List<String>();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Files (*.*)|*.*";
+            openFileDialog.Title = "Select vSphere Files";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string sFileName = openFileDialog.FileName;
+                string[] arrAllFiles = openFileDialog.FileNames;
+                foreach (string file in arrAllFiles)
+                {
+                    itemList.Add(file);
+                }
+                lstItems.ItemsSource = itemList;
+            }
+
+            if (lstItems != null)
+            {
+                btnNext.IsEnabled = true;
+                btnNext.Visibility = Visibility.Visible;
+            }
         }
     }
 }
+
+//itemList.Add(openFileDialog.Title.ToString());
+//lstItems.ItemsSource = itemList;

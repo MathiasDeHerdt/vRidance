3eea4743d43061f3cacbbe0764b9026d6898c807
Simon Demuynck <demuyncks1999@gmail.com>
2022-01-11 13:47:57 +0100

    Checking which checkboxes are checked, then handling code through a switchcase for conversion

--- vRidance/vRidance/MigrateSelection.xaml.cs
+++ vRidance/vRidance/MigrateSelection.xaml.cs
@@ -27,7 +27,50 @@ namespace vRidance
 
         private void btnNext2_Click(object sender, RoutedEventArgs e)
         {
-            
+            CheckBox[] checkBoxes = new CheckBox[4];
+            checkBoxes[0] = chckProxmox;
+            checkBoxes[1] = chckNutanix;
+            checkBoxes[2] = chckCitrix;
+            checkBoxes[3] = chckAzureStack;
+
+            for (int i = 0; i <= checkBoxes.Count() - 1; i++)
+            {
+
+                if (checkBoxes[i].IsChecked == true && checkBoxes[i].IsEnabled)
+                {
+                    ConvertToPlatform(i);
+                }
+
+            }
+
+        }
+
+        public void ConvertToPlatform(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    //CONVERTEREN GEBEURD HIER (PROXMOX)
+                    MessageBox.Show("PROXMOX is checked");
+                    break;
+
+                case 1:
+                    //CONVERTEREN GEBEURD HIER (NUTANIX)
+                    MessageBox.Show("NUTANIX is checked");
+                    break;
+
+                case 2:
+                    //CONVERTEREN GEBEURD HIER (CITRIX)
+                    MessageBox.Show("CITRIX is checked");
+                    break;
+                case 3:
+                    //CONVERTEREN GEBEURD HIER (AZURESTACKHCI)
+                    MessageBox.Show("AZURESTACKHCI is checked");
+                    break;
+                default:
+                    //error catch (no value)
+                    break;
+            }
         }
     }
 }

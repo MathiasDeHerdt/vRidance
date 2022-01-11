de560d46847b95cab221e9900d0b906ed94fa5a1
Ward Vandale <ward.vandale@student.howest.be>
2022-01-11 23:59:08 +0100

    My Takes on a UI

---
+++ vRidanceWinForms/vRidance/vRidance/MainWindow.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace vRidance
+{
+    partial class grdMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDarkTheme = new System.Windows.Forms.Label();
+            this.btnNext = new vRidance.CustomControls.CustomButton();
+            this.btnUpload = new vRidance.CustomControls.CustomButton();
+            this.chkbxTheme = new vRidance.CustomControls.CustomToggle();
+            this.SuspendLayout();
+            // 
+            // lblDarkTheme
+            // 
+            this.lblDarkTheme.AutoSize = true;
+            this.lblDarkTheme.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDarkTheme.Location = new System.Drawing.Point(660, 16);
+            this.lblDarkTheme.Name = "lblDarkTheme";
+            this.lblDarkTheme.Size = new System.Drawing.Size(77, 15);
+            this.lblDarkTheme.TabIndex = 1;
+            this.lblDarkTheme.Text = "Dark Theme";
+            this.lblDarkTheme.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnNext.BackgroundColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnNext.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnNext.BorderRadius = 13;
+            this.btnNext.BorderSize = 0;
+            this.btnNext.Enabled = false;
+            this.btnNext.FlatAppearance.BorderSize = 0;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.ForeColor = System.Drawing.Color.White;
+            this.btnNext.Location = new System.Drawing.Point(400, 400);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(150, 40);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.TextColor = System.Drawing.Color.White;
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnUpload.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnUpload.BorderColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnUpload.BorderRadius = 13;
+            this.btnUpload.BorderSize = 0;
+            this.btnUpload.FlatAppearance.BorderSize = 0;
+            this.btnUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpload.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.ForeColor = System.Drawing.Color.White;
+            this.btnUpload.Location = new System.Drawing.Point(248, 400);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(150, 40);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "UPLOAD";
+            this.btnUpload.TextColor = System.Drawing.Color.White;
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // chkbxTheme
+            // 
+            this.chkbxTheme.AutoSize = true;
+            this.chkbxTheme.Location = new System.Drawing.Point(743, 12);
+            this.chkbxTheme.MinimumSize = new System.Drawing.Size(45, 22);
+            this.chkbxTheme.Name = "chkbxTheme";
+            this.chkbxTheme.OffBackColor = System.Drawing.Color.Gray;
+            this.chkbxTheme.OffToggleColor = System.Drawing.Color.Gainsboro;
+            this.chkbxTheme.OnBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.chkbxTheme.OnToggleColor = System.Drawing.Color.WhiteSmoke;
+            this.chkbxTheme.Size = new System.Drawing.Size(45, 22);
+            this.chkbxTheme.TabIndex = 0;
+            this.chkbxTheme.UseVisualStyleBackColor = true;
+            this.chkbxTheme.CheckedChanged += new System.EventHandler(this.chkbxTheme_CheckedChanged);
+            // 
+            // grdMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.lblDarkTheme);
+            this.Controls.Add(this.chkbxTheme);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "grdMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "vRidance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CustomControls.CustomToggle chkbxTheme;
+        private System.Windows.Forms.Label lblDarkTheme;
+        private CustomControls.CustomButton btnUpload;
+        private CustomControls.CustomButton btnNext;
+    }
+}
+
---
+++ vRidanceWinForms/vRidance/vRidance/MainWindow.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Microsoft.Win32;
+using System.Windows;
+
+namespace vRidance
+{
+    public partial class grdMain : Form
+    {
+        Label[] arrAllLabels;
+        //light Theme Colors
+        Color LightThemeForm = SystemColors.ControlLightLight;
+        Color LightThemeButtonBG = Color.MediumSlateBlue;
+        Color LightThemeButtonBGDisabled = Color.DarkSlateBlue;
+        Color LightThemeFontColor = Color.Black;
+
+        //Dark Theme Colors
+        Color DarkThemeForm = Color.FromArgb(33, 33, 33);
+        Color DarkThemeButtonBG = Color.MediumSlateBlue;
+        Color DarkThemeButtonBGDisabled = Color.DarkSlateBlue;
+        Color DarkThemeFontColor = Color.White;
+
+        List<String> itemList = new List<String>();
+        public grdMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkbxTheme_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkbxTheme.Checked)
+            {
+                this.BackColor = DarkThemeForm;
+                //lstItems.BackColor = DarkThemeForm;
+
+
+                btnUpload.BackColor = DarkThemeButtonBG;
+
+                if (btnNext.Enabled == true) btnNext.BackColor = DarkThemeButtonBG;
+                else btnNext.BackColor = DarkThemeButtonBGDisabled;
+
+                lblDarkTheme.ForeColor = DarkThemeFontColor;
+                if (arrAllLabels != null)
+                {
+                    foreach (Label item in arrAllLabels)
+                    {
+                        item.ForeColor = DarkThemeFontColor;
+                    }
+                }
+                
+            }
+            else
+            {
+                this.BackColor = LightThemeForm;
+                //lstItems.BackColor = LightThemeForm;
+
+                btnUpload.BackColor = LightThemeButtonBG;
+
+                if (btnNext.Enabled == true) btnNext.BackColor = LightThemeButtonBG;
+                else btnNext.BackColor = LightThemeButtonBGDisabled;
+
+                lblDarkTheme.ForeColor = LightThemeFontColor;
+
+                if (arrAllLabels != null)
+                {
+                    foreach (Label item in arrAllLabels)
+                    {
+                        item.ForeColor = LightThemeFontColor;
+                    }
+                }
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Files (*.*)|*.*";
+            openFileDialog.Title = "Select vSphere Files";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = true;
+
+            try
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string sFileName = openFileDialog.FileName;
+                    string[] arrAllFiles = openFileDialog.FileNames;
+                    int i = 0;
+                    int nextLbl = 50;
+                    int labelHeight = 25;
+                    arrAllLabels = new Label[arrAllFiles.Length];
+                    foreach (string file in arrAllFiles)
+                    {
+                        string nameoffile = Path.GetFileName(file);
+                        arrAllLabels[i] = new Label();
+                        arrAllLabels[i].Name = "lblFile" + i;
+                        arrAllLabels[i].Size = new System.Drawing.Size(816, labelHeight);
+                        arrAllLabels[i].Location = new System.Drawing.Point(0, nextLbl);
+                        arrAllLabels[i].Margin = new System.Windows.Forms.Padding(10, 0, 10 , 0);
+                        arrAllLabels[i].Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        arrAllLabels[i].Text = nameoffile;
+                        arrAllLabels[i].TabIndex = 8;
+                        if (chkbxTheme.Checked) arrAllLabels[i].ForeColor = DarkThemeFontColor;
+                        else arrAllLabels[i].ForeColor = LightThemeFontColor;
+                        arrAllLabels[i].TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                        this.Controls.Add(arrAllLabels[i]);
+
+                        i++;
+                        nextLbl += labelHeight;
+                    }
+                    btnNext.Enabled = true;
+                    if (chkbxTheme.Checked) btnNext.BackColor = DarkThemeButtonBG;
+                    else btnNext.BackColor = LightThemeButtonBG;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            MigrateSelection migratepage = new MigrateSelection();
+            this.IsMdiContainer = true;
+            migratepage.MdiParent = this;
+            migratepage.Show();
+        }
+    }
+}
